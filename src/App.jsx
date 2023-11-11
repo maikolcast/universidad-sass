89449c6a1e023b7c9037a020f8dfcebfcac25b00
maikolcast <maikol.castano1991@gmail.com>
2023-11-11 18:09:20 -0500

    integracion con sass
    * crear tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./assets/scss/app.scss";
+import logo from "./assets/react.svg"; // Asegúrate de que esta ruta sea correcta
+
+const estudiantes = [
+  {
+    nombre: "Maikol Castaño",
+    carrera: "Ingeniería de Sistemas",
+    materia: "Desarrollo de software WEB Front-end",
+  },
+  {
+    nombre: "Milena Chavez",
+    carrera: "Ingeniería de Sistemas",
+    materia: "Desarrollo de software WEB Front-end",
+  },
+  {
+    nombre: "Juan Perez",
+    carrera: "Ingeniería de Sistemas",
+    materia: "Desarrollo de software WEB Front-end",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="contenedor">
+      <img src={logo} alt="Logo" className="logo" />
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>Estudiante</th>
+            <th>Carrera</th>
+            <th>Materia</th>
+          </tr>
+        </thead>
+        <tbody>
+          {estudiantes.map((est, i) => (
+            <tr key={i}>
+              <td>{est.nombre}</td>
+              <td>{est.carrera}</td>
+              <td>{est.materia}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
